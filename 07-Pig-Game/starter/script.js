98cfbb72c3f496204a9e09fe5142d7c0c3b47bc4
alexduffdesign <alexduffblog@gmail.com>
2022-09-29 16:21:17 +0100

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -10,9 +10,18 @@ const scoreEl0 = document.querySelector('#score--0');
 const scoreEl1 = document.querySelector('#score--1');
 
 // Dice
-const dice = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 
 // Default values
 scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
 dice.classList.add('hidden');
+
+// Clicking Roll
+diceRoll.addEventListener('click', function () {
+  // 1 - Generating random dice number between 1-6
+  let dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2 - Show that number on the dice
+  diceEl.src = `dice-${dice}.png`;
+});
