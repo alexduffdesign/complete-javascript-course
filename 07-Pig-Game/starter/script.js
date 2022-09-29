d27572f4d4b02222803454248fc8fd6c06660072
alexduffdesign <alexduffblog@gmail.com>
2022-09-29 16:14:40 +0100

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// Buttons
 const diceRoll = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
 const newGame = document.querySelector('.btn--new');
+
+// Players score
+const scoreEl0 = document.querySelector('#score--0');
+const scoreEl1 = document.querySelector('#score--1');
+
+// Dice
+const dice = document.querySelector('.dice');
+
+// Default values
+scoreEl0.textContent = 0;
+scoreEl1.textContent = 0;
+dice.classList.add('hidden');
